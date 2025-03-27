5de8794271adf96c4061166fc9b1ec65ad3152ff
Mohammad Faraji <m.faraji9841@gmail.com>
2025-03-27 14:52:15 +0330

    Add project files.

---
+++ SampleAppMVC_Identity/Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SampleAppMVC_Identity.Data.Models;
+using System;
+
+namespace SampleAppMVC_Identity.Data.Context
+{
+    // اضافه کردن فیلدهای اضافه با کلاس AppicationUser نوسط IdentityDbContext به مدل user
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser >
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options)
+        {
+        }
+
+    }
+}
---
+++ SampleAppMVC_Identity/Data/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SampleAppMVC_Identity.Data.Models
+{
+    // ایجاد فیلد سفارشی در مدل ApplicationUser به واسطه IdentityUser
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}
